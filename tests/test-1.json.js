f1f024d2162799c9d933b0b30d44f63d3a22e418
kel <kel@crk.umn.edu>
2014-10-02 14:23:24 -0500

    Add Test Cases

---
+++ tests/test-1.json.js
@@ -0,0 +1,30 @@
+var fields = 
+            [
+              {
+                'name':'name',
+                'id':'name',
+                'label': 'Student Name',
+                'type':'text',
+                'helpblock':'Last, First, Middle',
+                'maxLength':255,
+                'required':true,
+              },
+              {
+                  'name':'email',
+                  'id':'email',
+                  'label': 'Student email',
+                  'type':'email',
+                  'helpblock':'enter email',
+                  'maxLength':255,
+                  'required':true,
+                },
+                {
+                	'name':'test1',
+                	'label':'yes',
+                	type:'checkbox',
+                	checkboxes: [{label:'yes'},{label:'no'}]
+                	
+                }
+                
+            ]
+        ;
